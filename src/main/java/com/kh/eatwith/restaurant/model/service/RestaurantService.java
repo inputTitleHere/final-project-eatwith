54ee574f904b46c24ccbf2b564b2956d66c16860
YukyungShinn <syk6715@naver.com>
2022-09-02 21:48:01 +0900

    모임등록시 검색 data불러오기

---
+++ src/main/java/com/kh/eatwith/restaurant/model/service/RestaurantService.java
@@ -0,0 +1,11 @@
+package com.kh.eatwith.restaurant.model.service;
+
+import java.util.List;
+
+import com.kh.eatwith.restaurant.model.dto.Restaurant;
+
+public interface RestaurantService {
+
+	List<Restaurant> findRestaurantByName(String searchString);
+
+}
