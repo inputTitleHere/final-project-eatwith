1c93da0b62246252f260c14d06b2c33e76266d7f
inputTitleHere <seanbryan97@naver.com>
2022-09-28 15:36:56 +0900

    이클립스 인덱스 오류 js -> react 관련 주석처리

--- frontend/react/src/index.js
+++ frontend/react/src/index.js
@@ -5,13 +5,13 @@ import Mypage from './mypage-package/mypage-components/Mypage.jsx';
 // import MemberEnrollFrm from "./register-package/MemberEnrollFrm.jsx";
 // import Index from "./index-package/Index.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById("content-root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Mypage />
-    </BrowserRouter>
-    {/* <Index/> */}
-    {/* <MemberEnrollFrm/> */}
-  </React.StrictMode>
-);
+//const root = ReactDOM.createRoot(document.getElementById("content-root"));
+//root.render(
+//  <React.StrictMode>
+//    <BrowserRouter>
+//      <Mypage />
+//    </BrowserRouter>
+//    {/* <Index/> */}
+// 		  {/* <MemberEnrollFrm/> */}
+//  </React.StrictMode>
+//);
